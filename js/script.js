ece21958d0316c7fab20779ba6e6b64bbee6b88b
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-10-27 16:06:04 +0200

    add task to array tasks

--- js/script.js
+++ js/script.js
@@ -3,39 +3,42 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-            index: 0,
-            tooShort : false,
+
+            //oggetto tempo
+            newTask : {
+                text: "",
+                done: false
+            },
+
             //array nel quale pusherò gli oggetti task
             tasks:[
-                    {
-                        text: "",
-                        done: false
-                    },
+
                 ],
+
+                index: 0,
+                tooShort : false,
         }
     },
     methods:{
      addNewTask(){
          //pushare task solo se si hanno più di 5 caratteri
-        if(this.tasks[this.index].text.length <= 5){
+        if(this.newTask.text.length <= 5){
             this.tooShort = true;
 
         }else{
-            this.tasks.push(this.tasks[this.index]);
-            this.index++
-
-            //dopo il push puliamo il campo dell'input
-            this.tasks[this.index].text = ""; //non funziona
+            this.tasks.push({...this.newTask});
+            this.newTask.text = "";
+            this.newTask.done = false;
             this.tooShort = false;
         }
     },
     //funzione che elimina task (sostituisce un elemento dell'array con uno vuoto)
     removeTask(indexOfTask){
         this.tasks.splice(indexOfTask, 1);
-        this.index--
     },
+
     taskCompleted(){
-        this.tasks[index].done = !this.tasks[index].done
+        this.tasks[this.index].done = !this.tasks[this.index].done
     }
 
     }
